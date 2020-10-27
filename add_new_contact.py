1cc08b34de365bd00003115eea0d19275606f8c5
QuepasaSen <agentpnz90@bk.ru>
2020-10-27 22:41:37 +0300

    все функции параметризованы, также добавлен вспомогательный класс

--- add_new_contact.py
+++ add_new_contact.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
+from dop_info import Dop
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -14,24 +15,24 @@ class AddNewContact(unittest.TestCase):
     def test_add_new_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.add_new(wd)
-        self.fio_fields(wd)
-        self.about_company_fields(wd)
-        self.telephones_fields(wd)
-        self.emails_fields(wd)
-        self.homepage_field(wd)
+        self.fio_fields(wd, firstname="Petrov", middlename="Petr", lastname="Petrovich", nickname="Petya")
+        self.about_company_fields(wd, title="MyTitle", company="MyCompany", address="Russia")
+        self.telephones_fields(wd, domashniy="01-01-01", mobilniy="02-02-02", rabochiy="03-04-05", fax="06-07-08")
+        self.emails_fields(wd, email="test@mail.ru", email2="a@test.ru", email3="b@test.ru")
+        self.homepage_field(wd, "www.test.ru")
         self.birthday_fields(wd)
         self.anniversary_fields(wd)
-        self.additional_fields(wd)
+        self.additional_fields(wd, Dop(dop_address="USA", dop_phone="Colorado", notes="My notes about work"))
         self.back_to_home_page(wd)
         self.logout(wd)
 
-    def additional_fields(self, wd, dop_address="USA", dop_phone="Colorado", notes="My notes about work"):
+    def additional_fields(self, wd, dop_info):
         wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").send_keys(dop_address)
-        wd.find_element_by_name("phone2").send_keys(dop_phone)
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("address2").send_keys(dop_info.dop_address)
+        wd.find_element_by_name("phone2").send_keys(dop_info.dop_phone)
+        wd.find_element_by_name("notes").send_keys(dop_info.notes)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def back_to_home_page(self, wd):
@@ -63,29 +64,29 @@ class AddNewContact(unittest.TestCase):
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").send_keys("2020")
 
-    def homepage_field(self, wd):
-        wd.find_element_by_name("homepage").send_keys("www.test.ru")
+    def homepage_field(self, wd, homepage):
+        wd.find_element_by_name("homepage").send_keys(homepage)
 
-    def emails_fields(self, wd, email="test@mail.ru", email2="a@test.ru", email3="b@test.ru"):
+    def emails_fields(self, wd, email, email2, email3):
         wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_name("email2").send_keys(email2)
         wd.find_element_by_name("email3").send_keys(email3)
 
-    def telephones_fields(self, wd, domashniy="01-01-01", mobilniy="02-02-02", rabochiy="03-04-05", fax="06-07-08"):
+    def telephones_fields(self, wd, domashniy, mobilniy, rabochiy, fax):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").send_keys(domashniy)
         wd.find_element_by_name("mobile").send_keys(mobilniy)
         wd.find_element_by_name("work").send_keys(rabochiy)
         wd.find_element_by_name("fax").send_keys(fax)
 
-    def about_company_fields(self, wd, title="MyTitle", company="MyCompany", address="Russia"):
+    def about_company_fields(self, wd, title, company, address):
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").send_keys(title)
         wd.find_element_by_name("company").send_keys(company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").send_keys(address)
 
-    def fio_fields(self, wd, firstname="Petrov", middlename="Petr", lastname="Petrovich", nickname="Petya"):
+    def fio_fields(self, wd, firstname, middlename, lastname, nickname):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").send_keys(middlename)
@@ -95,11 +96,11 @@ class AddNewContact(unittest.TestCase):
     def add_new(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
---
+++ dop_info.py
@@ -0,0 +1,6 @@
+class Dop:
+
+    def __init__(self, dop_address, dop_phone, notes):
+        self.dop_address = dop_address
+        self.dop_phone = dop_phone
+        self.notes = notes
