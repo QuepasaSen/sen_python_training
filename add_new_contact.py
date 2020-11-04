62d0bd67b039a72d73fd5ff2ed861595b6efcf3d
QuepasaSen <agentpnz90@bk.ru>
2020-11-05 01:23:31 +0300

    перевёл тесты добавления контактов на pytest + фикстура согласно видеоуроку

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,114 +1,24 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
 from all_info import Info
+from applic import Applic
+import pytest
 
-class AddNewContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_add_new_contact(self):
-        wd = self.wd
-        infoclass = Info("USA", "Colorado", "My notes about work", "My notes about work", "a@test.ru", "b@test.ru", "01-01-01", "02-02-02", "03-04-05", "06-07-08", "MyTitle", "MyCompany", "Russia", "Petrov", "Petr", "Petrovich", "Petya")
-        self.login(wd, username="admin", password="secret")
-        self.add_new(wd)
-        self.fio_fields(wd, infoclass)
-        self.about_company_fields(wd, infoclass)
-        self.telephones_fields(wd, infoclass)
-        self.emails_fields(wd, infoclass)
-        self.homepage_field(wd, "www.test.ru")
-        self.birthday_fields(wd)
-        self.anniversary_fields(wd)
-        self.additional_fields(wd, infoclass)
-        self.back_to_home_page(wd)
-        self.logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Applic()
+    request.addfinalizer(fixture.destr)
+    return fixture
 
-    def additional_fields(self, wd, all_info):
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").send_keys(all_info.dop_address)
-        wd.find_element_by_name("phone2").send_keys(all_info.dop_phone)
-        wd.find_element_by_name("notes").send_keys(all_info.notes)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def back_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def anniversary_fields(self, wd):
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("2")
-        wd.find_element_by_xpath("(//option[@value='2'])[2]").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("April")
-        wd.find_element_by_xpath("(//option[@value='April'])[2]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").send_keys("2030")
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("ayear").click()
-
-    def birthday_fields(self, wd):
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
-        wd.find_element_by_xpath("//option[@value='1']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("April")
-        wd.find_element_by_xpath("//option[@value='April']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys("2020")
-
-    def homepage_field(self, wd, homepage):
-        wd.find_element_by_name("homepage").send_keys(homepage)
-
-    def emails_fields(self, wd, all_info):
-        wd.find_element_by_name("email").send_keys(all_info.email)
-        wd.find_element_by_name("email2").send_keys(all_info.email2)
-        wd.find_element_by_name("email3").send_keys(all_info.email3)
-
-    def telephones_fields(self, wd, all_info):
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").send_keys(all_info.domashniy)
-        wd.find_element_by_name("mobile").send_keys(all_info.mobilniy)
-        wd.find_element_by_name("work").send_keys(all_info.rabochiy)
-        wd.find_element_by_name("fax").send_keys(all_info.fax)
-
-    def about_company_fields(self, wd, all_info):
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").send_keys(all_info.title)
-        wd.find_element_by_name("company").send_keys(all_info.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").send_keys(all_info.address)
-
-    def fio_fields(self, wd, all_info):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys(all_info.firstname)
-        wd.find_element_by_name("middlename").send_keys(all_info.middlename)
-        wd.find_element_by_name("lastname").send_keys(all_info.lastname)
-        wd.find_element_by_name("nickname").send_keys(all_info.nickname)
-
-    def add_new(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_new_contact(app):
+    infoclass = Info("USA", "Colorado", "My notes about work", "My notes about work", "a@test.ru", "b@test.ru", "01-01-01", "02-02-02", "03-04-05", "06-07-08", "MyTitle", "MyCompany", "Russia", "Petrov", "Petr", "Petrovich", "Petya")
+    app.login(username="admin", password="secret")
+    app.add_new()
+    app.fio_fields(infoclass)
+    app.about_company_fields(infoclass)
+    app.telephones_fields(infoclass)
+    app.emails_fields(infoclass)
+    app.homepage_field("www.test.ru")
+    app.birthday_fields()
+    app.anniversary_fields()
+    app.additional_fields(infoclass)
+    app.back_to_home_page()
+    app.logout()
