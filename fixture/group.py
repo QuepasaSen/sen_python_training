8b184cde00908e76e8c9f68cfeb01553331d31e5
QuepasaSen <agentpnz90@bk.ru>
2020-11-05 23:19:42 +0300

    добавил тест для изменения группы

--- fixture/group.py
+++ fixture/group.py
@@ -34,6 +34,19 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # edit first group
+        wd.find_element_by_name("edit").click()
+        # rename first group
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("testing")
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
