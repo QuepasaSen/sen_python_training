d73d4128ec7bfae0f1d57c51a6d09e05ad407bf2
QuepasaSen <agentpnz90@bk.ru>
2020-11-05 22:53:52 +0300

    фикстура общая для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -9,6 +9,7 @@ class GroupHelper:
 
     def create(self, group):
         wd = self.app.wd
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -23,6 +24,15 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
 
     def open_groups_page(self):
         wd = self.app.wd
