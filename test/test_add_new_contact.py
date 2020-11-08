3bd2ac7c3a06c0ccd0047df8d009720ed59be836
QuepasaSen <agentpnz90@bk.ru>
2020-11-09 00:54:44 +0300

    добавили проверку валидности фикстуры

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,15 +1,5 @@
 from model.all_info import Info
-from fixture.application import Application
-import pytest
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_new_contact(app):
     infoclass = Info("USA", "Colorado", "My notes about work", "My notes about work", "a@test.ru", "b@test.ru", "01-01-01", "02-02-02", "03-04-05", "06-07-08", "MyTitle", "MyCompany", "Russia", "Petrov", "Petr", "Petrovich", "Petya")
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(infoclass)
-    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -19,6 +19,4 @@ def test_edit_contact(app):
         "Ivan",
         "Ivanovich",
         "Vanya")
-    app.session.login(username="admin", password="secret")
     app.contact.edit_contact(contact)
-    app.session.logout()
