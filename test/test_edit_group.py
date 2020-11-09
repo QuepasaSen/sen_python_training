223adba39925f413478d62c8e6596594d45779ff
QuepasaSen <agentpnz90@bk.ru>
2020-11-09 17:41:39 +0300

    Обеспечил выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,6 +7,6 @@ def test_edit_group_name(app):
 
 def test_edit_group_header(app):
     if app.group.count_of_group() == 0:
-        app.group.create_group(Group(name="test"))
+        app.group.create_group(Group(header="test"))
     app.group.edit_first_group(Group(header="New Header"))
 
