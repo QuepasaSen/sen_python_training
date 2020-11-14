22f9b545ec908beaf205de80d4a5cd6ad1456da1
QuepasaSen <agentpnz90@bk.ru>
2020-11-14 23:47:56 +0300

    добавили проверки на соответствие старого и нового списка при добавлении редактировании и удалении групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 def test_group_add_ff(app):
+    old_groups = app.group.get_group_list()
     app.group.create_group(Group(name="mynew", header="11", footer="22"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_empty_group_add_ff(app):
+    old_groups = app.group.get_group_list()
     app.group.create_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,12 +1,18 @@
 from model.group import Group
 
 def test_edit_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_of_group() == 0:
         app.group.create_group(Group(name="test"))
     app.group.edit_first_group(Group(name="New Name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_of_group() == 0:
         app.group.create_group(Group(header="test"))
     app.group.edit_first_group(Group(header="New Header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
