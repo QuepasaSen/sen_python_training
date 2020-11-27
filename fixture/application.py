9287d899ee247efb43c30571176c220975245753
QuepasaSen <agentpnz90@bk.ru>
2020-11-28 00:27:43 +0300

    ошибка запуска из консоли

--- fixture/application.py
+++ fixture/application.py
@@ -5,8 +5,15 @@ from fixture.group import GroupHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser="firefox"):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
